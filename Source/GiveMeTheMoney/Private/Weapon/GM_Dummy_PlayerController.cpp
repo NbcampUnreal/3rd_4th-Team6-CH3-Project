ec0d9815849566658f01fdb99d1d586d1a4a720c
JunCo <jkjun1234@gmail.com>
2025-08-04 15:42:29 +0900

    GM_Dummy_GameMode 클래스 추가 GM_Dummy_PlayerController 클래스 추가 DummyCharacter 이동 구현용으로 추가하였습니다.

---
+++ Source/GiveMeTheMoney/Private/Weapon/GM_Dummy_PlayerController.cpp
@@ -0,0 +1,2 @@
+#include "Weapon/GM_Dummy_PlayerController.h"
+
---
+++ Source/GiveMeTheMoney/Public/Weapon/GM_Dummy_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GM_Dummy_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GIVEMETHEMONEY_API AGM_Dummy_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
